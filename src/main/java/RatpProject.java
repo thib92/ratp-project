afdedcd082ba575be9ce9d33024f52f1a363c0df
thib92 <thibhenry92@gmail.com>
2019-06-09 22:30:31 +0200

    Optimize stream manipulation

--- src/main/java/RatpProject.java
+++ src/main/java/RatpProject.java
@@ -5,7 +5,7 @@ import me.tongfei.progressbar.ProgressBarBuilder;
 import network.JsonNetworkParser;
 import network.dto.Network;
 import network.dto.Station;
-import util.StationPair;
+import util.Pair;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -17,29 +17,28 @@ public class RatpProject {
 
     private static Network network;
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException {
         JsonNetworkParser networkParser = new JsonNetworkParser();
         network = networkParser.parseNetwork("src/main/resources/reseau.json");
         Graph<Station> stationGraph = networkParser.buildGraph(network, false);
         Graph<Station> weightedStationGraph = networkParser.buildGraph(network, true);
 
-        Map.Entry<StationPair, List<Vertex<Station>>> longestPath = getLongestPath(weightedStationGraph);
-        StationPair stationPair = longestPath.getKey();
-        System.out.println(String.format("Longest path is between %s and %s", stationPair.getFirst(), stationPair.getSecond()));
+        Map.Entry<Pair, List<Vertex<Station>>> longestPath = getLongestPath(weightedStationGraph);
+        Pair stationPair = longestPath.getKey();
+        System.out.println(String.format("Longest path is between %s and %s", stationPair.getKey(), stationPair.getValue()));
     }
 
     /**
      * Find the shortest path for any directed pair of stations
      * @param stationGraph The directed graph with stations as nodes
      * @return A map of station pairs to the path from station A to station B
-     * @throws InterruptedException Thrown if the computation takes more than 10 minutes
      */
-    private static Map<StationPair, List<Vertex<Station>>> shortestPaths(Graph<Station> stationGraph) throws InterruptedException {
+    private static Map<Pair, List<Vertex<Station>>> shortestPaths(Graph<Station> stationGraph) {
         // Build a list of all stations pairs
-        List<StationPair> pairs = new ArrayList<>();
+        List<Pair<Station, Station>> pairs = new ArrayList<>();
         for (Station station: network.getStations().values()) {
             for (Station station2: network.getStations().values()) {
-                pairs.add(new StationPair(station, station2));
+                pairs.add(new Pair<>(station, station2));
             }
         }
 
@@ -51,16 +50,14 @@ public class RatpProject {
                         .showSpeed()
                         .build()
         ) {
-            Map<StationPair, List<Vertex<Station>>> shortestPaths = new HashMap<>();
+            return pairs.parallelStream()
+                    .map(stationPair -> {
+                        List<Vertex<Station>> shortestPath = stationGraph.dijkstra(stationPair.getKey(), stationPair.getValue());
+                        return new Pair<>(stationPair, shortestPath);
+                    })
+                    .peek((stationPair -> pb.step()))
+                    .collect(HashMap::new, (m, pair) -> m.put(pair.getKey(), pair.getValue()), HashMap::putAll);
 
-            pairs.parallelStream()
-                    .forEach(stationPair -> {
-                        List<Vertex<Station>> shortestPath = stationGraph.dijkstra(stationPair.getFirst(), stationPair.getSecond());
-                        shortestPaths.put(stationPair, shortestPath);
-                        pb.step();
-                    });
-
-            return shortestPaths;
         }
     }
 
@@ -68,13 +65,12 @@ public class RatpProject {
      * Find the diameter of a graph by computing all the shortest paths and finding the longest one
      * @param stationGraph The directed graph of which to find the diameter
      * @return A map from the Map Entry of the pair of stations with the longest shortest path to the path from station A to station B
-     * @throws InterruptedException Thrown if the computation takes more than 10 minutes
      */
-    private static Map.Entry<StationPair, List<Vertex<Station>>> getLongestPath(Graph<Station> stationGraph) throws InterruptedException {
-        Map<StationPair, List<Vertex<Station>>> shortestPaths = shortestPaths(stationGraph);
+    private static Map.Entry<Pair, List<Vertex<Station>>> getLongestPath(Graph<Station> stationGraph) {
+        Map<Pair, List<Vertex<Station>>> shortestPaths = shortestPaths(stationGraph);
 
-        Map.Entry<StationPair, List<Vertex<Station>>> longestPath = null;
-        for (Map.Entry<StationPair, List<Vertex<Station>>> entry: shortestPaths.entrySet()) {
+        Map.Entry<Pair, List<Vertex<Station>>> longestPath = null;
+        for (Map.Entry<Pair, List<Vertex<Station>>> entry: shortestPaths.entrySet()) {
             // List will be null if stations cannot be connected
             if (entry.getValue() == null) {
                 continue;
--- src/main/java/util/Pair.java
+++ src/main/java/util/Pair.java
@@ -2,14 +2,13 @@ package util;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import network.dto.Station;
 
 /**
  * Utility class to hold an ordered pair of Stations
  */
 @Data
 @AllArgsConstructor
-public class StationPair{
-    private Station first;
-    private Station second;
+public class Pair<K, V> {
+    private K key;
+    private V value;
 }
